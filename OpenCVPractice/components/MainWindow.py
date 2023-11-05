9b7fb86748d618893e29b9fc82577588b4874678
hushuhan <873933169@qq.com>
2023-11-05 16:29:24 +0800

    ui update

--- OpenCVPractice/components/MainWindow.py
+++ OpenCVPractice/components/MainWindow.py
@@ -3,7 +3,7 @@ from ui.ui_mainwindow import Ui_MainWindow
 
 
 class MainWindow(QMainWindow):
+    ui = Ui_MainWindow()
     def __init__(self):
         super().__init__()
-        self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
