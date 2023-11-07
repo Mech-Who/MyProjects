006308b59aa4d7d82989250fdeaa1baafde82f15
Mech_Hsh <873933169@qq.com>
2023-11-07 19:12:04 +0800

    单例模式代码错误改正

--- Patterns/SingletonPattern/Example2.py
+++ Patterns/SingletonPattern/Example2.py
@@ -2,7 +2,7 @@ class HealthCheck:
     _instance = None
     def __new__(cls, *args, **kwargs):
         if not HealthCheck._instance:
-            HealthCheck._instance = super(HealthCheck, cls).__new__(cls, *args, **kwargs):
+            HealthCheck._instance = super(HealthCheck, cls).__new__(cls, *args, **kwargs)
         return HealthCheck._instance
     def __init__(self):
         self._servers = []
