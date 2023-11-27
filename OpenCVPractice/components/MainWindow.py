a5002ba630fbaf8b5379a99ab5168b6768a56d1e
HuShuhan <873933169@qq.com>
2023-11-27 11:03:36 +0800

    split OpenCVPractice to Homework

--- OpenCVPractice/components/MainWindow.py
+++
@@ -1,9 +0,0 @@
-from PySide6.QtWidgets import QMainWindow
-from ui.ui_mainwindow import Ui_MainWindow
-
-
-class MainWindow(QMainWindow):
-    ui = Ui_MainWindow()
-    def __init__(self):
-        super().__init__()
-        self.ui.setupUi(self)
